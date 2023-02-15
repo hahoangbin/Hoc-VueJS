d30afc0a5a2d6a57fe84f4f93297ae6fe0f28c0b
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-02-15 22:03:24 +0700

    Add Bai-08 handleMousemove / submit

--- Bai-08/vue.js
+++ Bai-08/vue.js
@@ -1,80 +1,14 @@
-// Bai-02
-
-
-// const app = Vue.createApp({
-//     template:  `
-//     <h2>Hello from {{ teamName }} and {{ !isHello ? firsName + " " + lastName : "" }}</h2>
-
-//         <button @click="onChangeName">Change Name but shorter</button>
-
-//         <div class="modal" v-if="isShowModal">Modal content</div>
-//         `,
-//     data() {
-//         return{
-//             teamName: "RHP Team",
-//             firsName: 'Hà',
-//             lastName: 'Bin',
-//             isHello: false,
-//             isShowModal: false,
-//         }
-//     },
-//     methods: {
-//         onChangeName() {
-//             this.firsName = "Sky"
-//             this.lastName = "Albert"
-//             this.isShowModal = true
-//         }
-//     }
-// });
-
-// app.mount("#contact")
-
-//  Bai-03
-
-const app = Vue.createApp({
-    data() {
-        return {
-            urlTesting: "gg.com",
-            classTesting: "box",
-            products: [
-                {
-                    name: 'Sản phẩm A',
-                    price: '120$',
-                    thumb: './assets/1.jpg',
-                    isCart: false,
-                },
-                {
-                    name: 'Sản phẩm B',
-                    price: '150$',
-                    thumb: './assets/2.jpg',
-                    isCart: true,
-                },
-                {
-                    name: 'Sản phẩm C',
-                    price: '449$',
-                    thumb: './assets/3.jpg',
-                    isCart: false,
-                },
-                {
-                    name: 'Sản phẩm VIP',
-                    price: '999$',
-                    thumb: './assets/4.jpg',
-                    isCart: true,
-                }
-            ]
-        }
+var vueInstance = new Vue({
+    el: '#app',
+    data: {
+        title: 'Aó thun nam thể thao hàng VNXK vải dày mịn!',
+        url: 'https://imgs.search.brave.com/gbRmI6hiP91nrUPTwoQou1P0oL1DzAkdSqCqSYziCyg/rs:fit:800:1067:1/g:ce/aHR0cHM6Ly93d3cu/YWttZW4udm4vaW1h/Z2VzL3RodW1icy8y/MDE4LzExL2FvLXRo/dW4tdHJhbmctYXQ3/ODUtMTAxNTYtc2xp/ZGUtcHJvZHVjdHMt/NWJkYWMxOTA4YTBj/ZC5qcGc',
+        target: '_top',
+        price: 120000
     },
     methods: {
-        onToggleCart(event, product) {
-            product.isCart = !product.isCart
-        }
-    },
-    computed: {
-        productsComputed() {
-            return this.products.filter((product) => product.price < '500$')
+        say: function(text) {
+            return 'HELLO' + text;
         }
     }
-    
 })
-
-app.mount("#contact")
